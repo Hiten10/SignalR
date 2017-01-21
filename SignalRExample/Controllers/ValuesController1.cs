43dba4af25ac69e5a025cb885342aa1ab508f96d
Hiten10 <hitender100282@gmail.com>
2017-01-21 13:32:01 +0530

    https://github.com/Hiten10/SignalR.git

---
+++ SignalRExample/Controllers/ValuesController1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SignalRExample.Repository;
+
+namespace SignalRExample.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        JobInfoRepository objRepo = new JobInfoRepository();
+        // GET api/<controller>
+        public IEnumerable<JobInfo> Get()
+        {
+            return objRepo.GetData();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SignalRExample/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+
+namespace SignalRExample
+{
+    [assembly: OwinStartup(typeof(SignalRExample.Startup))]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
